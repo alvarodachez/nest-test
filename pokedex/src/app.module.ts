07265e4e91059a427e70b65737c4b9cb6e3288c4
BABEL-SI\alvaro.rueda <alvaro.rueda@babel.es>
2023-01-16 17:43:57 +0100

    pokedex added2

---
+++ pokedex/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
